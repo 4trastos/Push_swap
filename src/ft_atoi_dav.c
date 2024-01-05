7168f22afa183c9deebee5b10a38e78427e460b4
David Gallego Gomez <davgalle@c3r13s2.42madrid.com>
2024-01-05 12:45:14 +0100

    ok

--- src/ft_atoi_dav.c
+++ src/ft_atoi_dav.c
@@ -6,7 +6,7 @@
 /*   By: davgalle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 19:57:43 by davgalle          #+#    #+#             */
-/*   Updated: 2024/01/03 11:56:46 by davgalle         ###   ########.fr       */
+/*   Updated: 2024/01/05 12:40:11 by davgalle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,26 @@
 int	ft_atoi_dav(char *str, int *status)
 {
 	int				i;
-	int				valorfinal;
+	int				result;
 	long long int	nbr;
 
 	nbr = 0;
 	i = 0;
-	valorfinal = 1;
+	result = 1;
 	if (str[i] == '-' || str[i] == '+')
 	{
 		if (str[i] == '-')
-			valorfinal = -1;
+			result = -1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
 	{
 		nbr = nbr * 10 + str[i] - '0';
 		i++;
-		if (nbr > ((long long int)INT_MAX + 1) && valorfinal == -1)
+		if (nbr > ((long long int)INT_MAX + 1) && result == -1)
 			*status = 1;
-		if (nbr > INT_MAX && valorfinal == 1)
+		if (nbr > INT_MAX && result == 1)
 			*status = 1;
 	}
-	return (nbr * valorfinal);
+	return (nbr * result);
 }
